35fb55bfd3def4c8a343f99b5f7424f56df1692b
河田鷹介 <benefium@gmail.com>
2021-11-07 18:49:56 +0900

    0102

---
+++ ex01/Fixed.cpp
@@ -0,0 +1,70 @@
+#include "Fixed.hpp"
+#include <cmath>
+
+float   AbsoluteFloat(float v) {
+    return v >= 0 ? v : -v;
+}
+
+Fixed::Fixed(): bits_(0) {
+    std::cout << "Fixed() Called" << std::endl;
+}
+
+Fixed::Fixed(int const val){
+    std::cout << "Fixed(int const) Called" << std::endl;
+    int shifted = val << Fixed::kFractionalBits;
+    int reshifted = shifted >> Fixed::kFractionalBits;
+    if (val != reshifted) {
+        bits_ = val > 0 ? INT_MAX : INT_MIN;
+    } else {
+        bits_ = shifted;
+    }
+    std::cout
+        << shifted << std::endl
+        << reshifted << std::endl
+        << bits_ << std::endl;
+}
+
+Fixed::Fixed(float const val){
+    std::cout << "Fixed(float const) Called" << std::endl;
+    double fb2 = (double)(1 << Fixed::kFractionalBits);
+    bits_ = roundf(val * fb2);
+}
+
+Fixed::~Fixed() {
+    std::cout
+        << "~Fixed() Called: "
+        << "rawbit is: "
+        << bits_
+        << std::endl;
+}
+
+Fixed::Fixed(const Fixed &from) {
+    std::cout << "Fixed(const Fixed &from) Called" << std::endl;
+    bits_ = from.getRawBits();
+}
+
+Fixed &Fixed::operator=(const Fixed &rhs) {
+    std::cout << "= Called" << std::endl;
+    bits_ = rhs.getRawBits();
+    return *this;
+}
+
+int Fixed::getRawBits(void) const {
+    return bits_;
+}
+
+void    Fixed::setRawBits(int const raw) {
+    bits_ = raw;
+}
+
+int     Fixed::toInt(void) const {
+    return bits_ >> Fixed::kFractionalBits;
+}
+float   Fixed::toFloat(void) const {
+    double fb2 = (double)(1 << Fixed::kFractionalBits);
+    return bits_ / fb2;
+}
+
+std::ostream& operator<<(std::ostream& stream, const Fixed& value) {
+    return stream << value.toFloat();
+}
--- ex01/Fixed.hpp
+++ ex01/Fixed.hpp
@@ -6,17 +6,22 @@
 class Fixed {
     public:
         Fixed();
+        Fixed(int const val);
+        Fixed(float const val);
         ~Fixed();
         Fixed(const Fixed &from);
         Fixed& operator=(const Fixed &rhs);
 
         int     getRawBits(void) const;
         void    setRawBits(int const raw);
+        int     toInt(void) const;
+        float   toFloat(void) const;
 
     private:
         int                 bits_;
         static const int    kFractionalBits = 8;
-
 };
+
+std::ostream& operator<<(std::ostream& stream, const Fixed& value);
 
 #endif
